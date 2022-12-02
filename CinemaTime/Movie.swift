976ea9e467c72358edd5ec631c54795fede15769
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-02 13:20:59 +0100

    Add `Movie` business model

---
+++ CinemaTime/Movie.swift
@@ -0,0 +1,12 @@
+//  Created by Marcell Magyar on 02.12.22.
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let image: URL?
+    let overview: String?
+    let releaseDate: Date?
+    let rating: Double?
+}
