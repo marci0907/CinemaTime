9609992ab484941f49d5e0a905fa902dcfd9941f
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-02 13:09:51 +0100

    Initial Commit

---
+++ CinemaTime/CinemaTime.h
@@ -0,0 +1,18 @@
+//
+//  CinemaTime.h
+//  CinemaTime
+//
+//  Created by Marcell Magyar on 02.12.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CinemaTime.
+FOUNDATION_EXPORT double CinemaTimeVersionNumber;
+
+//! Project version string for CinemaTime.
+FOUNDATION_EXPORT const unsigned char CinemaTimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CinemaTime/PublicHeader.h>
+
+
