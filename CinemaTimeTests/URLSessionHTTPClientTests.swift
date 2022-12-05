5c27754b89244f87c1dad28c5ef59fdd59206f61
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-05 12:41:46 +0100

    `URLSessionHTTPClient` get requestsDataFromURL

--- CinemaTimeTests/URLSessionHTTPClientTests.swift
+++ CinemaTimeTests/URLSessionHTTPClientTests.swift
@@ -1,6 +1,7 @@
 //  Created by Marcell Magyar on 05.12.22.
 
 import XCTest
+import CinemaTime
 
 final class URLSessionHTTPClient {
     private let session: URLSession
@@ -8,9 +9,29 @@ final class URLSessionHTTPClient {
     init(session: URLSession) {
         self.session = session
     }
+    
+    struct Error: Swift.Error {}
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        let urlRequest = URLRequest(url: url)
+        
+        session.dataTask(with: urlRequest) { _, _, _ in
+            completion(.failure(Error()))
+        }.resume()
+    }
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        URLProtocol.registerClass(URLProtocolStub.self)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocol.unregisterClass(URLProtocolStub.self)
+        URLProtocolStub.reset()
+    }
     
     func test_init_doesNotExecuteRequest() {
         let config = URLSessionConfiguration.ephemeral
@@ -19,14 +40,64 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let session = URLSession(configuration: config)
         _ = URLSessionHTTPClient(session: session)
         
-        XCTAssertTrue(URLProtocolStub.receivedURLs.isEmpty)
+        XCTAssertTrue(URLProtocolStub.shared?.receivedURLs.isEmpty == true)
+    }
+    
+    func test_get_requestsDataFromURL() {
+        let url = URL(string: "https://any-url.com")!
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [URLProtocolStub.self]
+        
+        let session = URLSession(configuration: config)
+        let sut = URLSessionHTTPClient(session: session)
+        
+        let exp = expectation(description: "Wait for completion")
+        sut.get(from: url) { _ in
+            XCTAssertEqual(URLProtocolStub.shared?.receivedURLs, [url])
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: - Helpers
     
     private class URLProtocolStub: URLProtocol {
-        static let shared = URLProtocolStub()
+        static var shared: URLProtocolStub?
         
-        private(set) static var receivedURLs = [URL]()
+        override init(request: URLRequest, cachedResponse: CachedURLResponse?, client: URLProtocolClient?) {
+            super.init(request: request, cachedResponse: cachedResponse, client: client)
+            
+            URLProtocolStub.shared = self
+        }
+        
+        private var _receivedURLs = [URL]()
+        private let queue = DispatchQueue(label: "\(URLProtocolStub.self)Queue")
+        
+        var receivedURLs: [URL] {
+            get { queue.sync { _receivedURLs }}
+            set { queue.sync { _receivedURLs = newValue }}
+        }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+        
+        override func startLoading() {
+            receivedURLs.append(request.url!)
+            
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() {}
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
+        static func reset() {
+            shared?.receivedURLs = []
+        }
     }
 }
