db6e948eb89eacb96c3bf02d2696ef3bbca939b6
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-06 12:05:10 +0100

    `RemoteMovieLoader` delivers now playing movies with valid API key

--- CinemaTime/Movie/API/URLSessionHTTPClient.swift
+++ CinemaTime/Movie/API/URLSessionHTTPClient.swift
@@ -4,17 +4,22 @@ import Foundation
 
 public final class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
+    private let apiKey: String
     
     public typealias Result = HTTPClient.Result
     
-    public init(session: URLSession) {
+    public init(session: URLSession, apiKey: String) {
         self.session = session
+        self.apiKey = apiKey
     }
     
     private struct UnknownCaseRepresentation: Swift.Error {}
     
     public func get(from url: URL, completion: @escaping (Result) -> Void) {
-        let urlRequest = URLRequest(url: url)
+        var urlComponents = URLComponents(string: url.absoluteString)!
+        urlComponents.queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
+        
+        let urlRequest = URLRequest(url: urlComponents.url!)
         
         session.dataTask(with: urlRequest) { data, response, error in
             if let error = error {
--- CinemaTimeAPIEndToEndTests/CinemaTimeAPIEndToEndTests.swift
+++ CinemaTimeAPIEndToEndTests/CinemaTimeAPIEndToEndTests.swift
@@ -7,8 +7,8 @@ final class CinemaTimeAPIEndToEndTests: XCTestCase {
     
     func test_load_deliversInvalidDataErrorWithoutAPIKey() {
         let config = URLSessionConfiguration.ephemeral
-        let client = URLSessionHTTPClient(session: URLSession(configuration: config))
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1")!
+        let client = URLSessionHTTPClient(session: URLSession(configuration: config), apiKey: "")
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
         let remoteLoader = RemoteMovieLoader(url: url, client: client)
         
         let exp = expectation(description: "Wait for request completion")
@@ -19,6 +19,28 @@ final class CinemaTimeAPIEndToEndTests: XCTestCase {
                 
             default:
                 XCTFail("Expected invalidData error, received \(result)")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+    }
+    
+    func test_load_deliversNowPlayingMoviesWithValidAPIKey() {
+        let config = URLSessionConfiguration.ephemeral
+        let client = URLSessionHTTPClient(session: URLSession(configuration: config), apiKey: APIKey)
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
+        let remoteLoader = RemoteMovieLoader(url: url, client: client)
+        
+        let exp = expectation(description: "Wait for request completion")
+        remoteLoader.load { result in
+            switch result {
+            case let .success(movies):
+                XCTAssertEqual(movies.count, 20)
+                
+            default:
+                XCTFail("Expected success, received \(result)")
             }
             
             exp.fulfill()
