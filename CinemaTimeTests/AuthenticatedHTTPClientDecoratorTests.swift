88b9a7431eff2905514c030f33f48bc56cabc370
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-06 13:30:38 +0100

    `AuthenticatedHTTPClientDecorator` get requests data from URL

--- CinemaTimeTests/AuthenticatedHTTPClientDecoratorTests.swift
+++ CinemaTimeTests/AuthenticatedHTTPClientDecoratorTests.swift
@@ -11,7 +11,7 @@ final class AuthenticatedHTTPClientDecorator {
     }
     
     func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        
+        decoratee.get(from: url) { _ in }
     }
 }
 
@@ -22,5 +22,15 @@ final class AuthenticatedHTTPClientDecoratorTests: XCTestCase {
         _ = AuthenticatedHTTPClientDecorator(decoratee: client)
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_get_requestsDataFromURL() {
+        let url = anyURL()
+        let client = HTTPClientSpy()
+        let sut = AuthenticatedHTTPClientDecorator(decoratee: client)
+        
+        sut.get(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
     }
 }
