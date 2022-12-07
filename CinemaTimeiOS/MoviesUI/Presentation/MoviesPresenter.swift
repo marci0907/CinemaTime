a83c5f5b50db4862991894e6da5a568895d789b9
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-07 20:46:48 +0100

    Fix `loaderView` naming to `loadingView` in `MoviesPresenter`

--- CinemaTimeiOS/MoviesUI/Presentation/MoviesPresenter.swift
+++ CinemaTimeiOS/MoviesUI/Presentation/MoviesPresenter.swift
@@ -4,19 +4,19 @@ import CinemaTime
 
 final class MoviesPresenter {
     private let moviesView: MoviesView
-    private let loaderView: MoviesLoadingView
+    private let loadingView: MoviesLoadingView
     private let loader: MovieLoader
     
     init(moviesView: MoviesView, loadingView: MoviesLoadingView, loader: MovieLoader) {
         self.moviesView = moviesView
-        self.loaderView = loadingView
+        self.loadingView = loadingView
         self.loader = loader
     }
     
     func load() {
-        loaderView.display(MoviesLoadingViewModel(isLoading: true))
+        loadingView.display(MoviesLoadingViewModel(isLoading: true))
         loader.load { [weak self] result in
-            self?.loaderView.display(MoviesLoadingViewModel(isLoading: false))
+            self?.loadingView.display(MoviesLoadingViewModel(isLoading: false))
             if let movies = try? result.get() {
                 self?.moviesView.display(MoviesViewModel(movies: movies))
             }
