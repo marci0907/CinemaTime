abe531ac380b2d62b5d65dc903ba9ada0cbdc11c
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-12 21:36:51 +0100

    Add CinemaTimeApp xcodeproj and xcworkspace

---
+++ CinemaTimeApp/CinemaTimeApp/ViewController.swift
@@ -0,0 +1,14 @@
+//  Created by Marcell Magyar on 12.12.22.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CinemaTimeApp/CinemaTimeAppTests/CinemaTimeAppTests.swift
@@ -0,0 +1,7 @@
+//  Created by Marcell Magyar on 12.12.22.
+
+import XCTest
+
+final class CinemaTimeAppTests: XCTestCase {
+
+}
