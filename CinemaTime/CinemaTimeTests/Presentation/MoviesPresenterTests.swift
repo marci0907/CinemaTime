6aa61ea9ccabebbdb1a17d77895c166fca4a8c68
Marcell Magyar <marcell.magyar@netcetera.com>
2022-12-14 09:56:03 +0100

    `MoviesPresenter` load starts loading

--- CinemaTime/CinemaTimeTests/Presentation/MoviesPresenterTests.swift
+++ CinemaTime/CinemaTimeTests/Presentation/MoviesPresenterTests.swift
@@ -14,6 +14,15 @@ final class MoviesPresenterTests: XCTestCase {
         XCTAssertTrue(loader.receivedMovieLoads.isEmpty)
     }
     
+    func test_load_startsLoading() {
+        let view = ViewSpy()
+        let sut = MoviesPresenter(moviesView: view, loadingView: view, loader: LoaderSpy())
+        
+        sut.load()
+        
+        XCTAssertEqual(view.receivedMessages, [.display(isLoading: true)])
+    }
+    
     // MARK: -
     
     private class LoaderSpy: MovieLoader {
@@ -35,19 +44,19 @@ final class MoviesPresenterTests: XCTestCase {
     }
     
     private class ViewSpy: MoviesView, MoviesLoadingView {
-        enum Message {
-            case display(MoviesViewModel)
-            case displayLoading(MoviesLoadingViewModel)
+        enum Message: Equatable {
+            case display(movies: [Movie])
+            case display(isLoading: Bool)
         }
         
         private(set) var receivedMessages = [Message]()
         
         func display(_ viewModel: MoviesViewModel) {
-            receivedMessages.append(.display(viewModel))
+            receivedMessages.append(.display(movies: viewModel.movies))
         }
         
         func display(_ viewModel: MoviesLoadingViewModel) {
-            receivedMessages.append(.displayLoading(viewModel))
+            receivedMessages.append(.display(isLoading: viewModel.isLoading))
         }
     }
 }
